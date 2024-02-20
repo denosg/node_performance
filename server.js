60405be75e0c8d8ca1de896da04ad7cb8c5c51cc
denos <costelasdenis@gmail.com>
2024-02-20 18:25:35 +0200

    clustering

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 const express = require('express');
-const cluster = require('cluster');
+const cluster = require('node:cluster');
 
 const app = express()
 
 function delay(duration) {
     const startTime = Date.now()
-    while(Date.now() - startTime < duration){
+    while (Date.now() - startTime < duration) {
         //delay
     }
 }
 
 app.get('/', (req, res) => {
-    res.send('Performance Example')
+    res.send(`Performance Example: ${process.pid}`)
 })
 
 app.get('/timer', (req, res) => {
     delay(9000);
-    res.send('timer ding')
+    res.send(`timer ding: ${process.pid}`)
 })
 
-app.listen(3000)
+if (cluster.isPrimary) {
+    console.log(`Master has been started...`);
+    cluster.fork();
+    cluster.fork();
+} else {
+    console.log(`Worker Process Started.`);
+    app.listen(3000)
+}
+
